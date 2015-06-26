636aac245db1b577a4feb8e2a396495f4a24e555
aleksandra kazmierczak <kazmierczak@aleksandras-MacBook-Air.local>
2015-06-26 23:29:19 +0200

    index registration

--- this-bike-app/public/js/index.js
+++ this-bike-app/public/js/index.js
@@ -1,12 +1,29 @@
 var thisBikeApp = angular.module("thisBike", ['ui.bootstrap']);
 
-	thisBikeApp.controller("thisBikeCtrl", ['$scope', function ($scope) {
+	thisBikeApp.controller("thisBikeCtrl", ['$scope', function ($scope, $state, $http) {
 		console.log('dupa')
   		$scope.test = 'Hola!';
-		$scope.tabs = [
-			{ title:'Dynamic Title 1', content:'Dynamic content 1' },
-			{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-		];
+
+		$scope.user = [{
+			"login": "loginTest1",
+			"avatar":"avatar.jpg",
+			"bike": {
+			"photos": [
+				"img1.jpg",
+				"img2.jpg",
+				"img3.jpg"
+			],
+				"desc" : "Awesome bike bike bike bikebike",
+				"name" : "Mr-White"
+		},
+			"options" : {
+			"email":"testEmail@gmail.com",
+				"city":"Lodz"
+		}
+		}];
+
 	}]);
+
+
 
 
