d098591bdf8f31c690240a7fcbe88b04ec54564f
aleksandra kazmierczak <kazmierczak@aleksandras-MacBook-Air.local>
2015-06-26 22:43:51 +0200

    login form

--- this-bike-app/public/js/index.js
+++ this-bike-app/public/js/index.js
@@ -1,8 +1,12 @@
-var thisBikeApp = angular.module("thisBike", []);
+var thisBikeApp = angular.module("thisBike", ['ui.bootstrap']);
 
 	thisBikeApp.controller("thisBikeCtrl", ['$scope', function ($scope) {
 		console.log('dupa')
   		$scope.test = 'Hola!';
+		$scope.tabs = [
+			{ title:'Dynamic Title 1', content:'Dynamic content 1' },
+			{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+		];
 	}]);
 
 
