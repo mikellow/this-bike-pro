e813d39146bf4941d249a00e83ef80811ba08b67
aleksandra kazmierczak <kazmierczak@aleksandras-MacBook-Air.local>
2015-06-26 21:07:34 +0200

    angular not working

---
+++ this-bike-app/public/js/index.js
@@ -0,0 +1,8 @@
+var app = angular.module("thisBike", []);
+
+app.controller("thisBikeCtrl", ['$scope', function ($scope) {
+
+
+}]);
+
+
---
+++ this-bike-app/views/index.js
@@ -0,0 +1,10 @@
+var app = angular.module("thisBike", []);
+
+app.controller("thisBikeCtrl", ['$scope', function ($scope) {
+
+    $scope.test ='Beng!';
+    console.log('test');
+
+}]);
+
+
