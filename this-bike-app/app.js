4dcc4721cfb748fe65469077e8c4b7d89b65c270
aleksandra kazmierczak <kazmierczak@aleksandras-MacBook-Air.local>
2015-06-27 00:48:50 +0200

    show-page

--- this-bike-app/app.js
+++ this-bike-app/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users-page', users);
+app.use('/user-page', users);
 app.use('/sign-page', sign);
 app.use('/show-page', showBike);
 
--- this-bike-app/public/js/show-page.js
+++ this-bike-app/public/js/show-page.js
@@ -1,3 +1,100 @@
-/**
- * Created by kazmierczak on 26/06/15.
- */
+var thisBikeApp = angular.module("thisBike", ['ui.bootstrap']);
+
+thisBikeApp.controller("showPageCtrl", ['$scope', function ($scope, $state, $http) {
+    console.log('dupa')
+    $scope.test = 'Hola!';
+    $scope.foto = 'http://placekitten.com/g/200/300';
+
+    $scope.user = [{
+        "login": "loginTest1",
+        "avatar":"avatar.jpg",
+        "bike": {
+            "photos": [
+                "http://www.pedalroom.com/p/mr-fix-it-7819_1.jpg",
+                "http://image.made-in-china.com/2f0j00kMltwycaljrO/700c-New-Model-Hot-Sale-Specialized-White-Fixed-Gear-Bike-ZLF-2013S-.jpg",
+                "http://www.czytajniepytaj.pl/data/domains/1/pl/filemanager_files/accounts/lena/3/rower/pure-fix-cycles-bikes-4.jpg",
+                "http://static.jensonusa.com/images/Default-Image/Zoom/4/BI242F00.jpg"
+            ],
+            "desc" : "Awesome bike bike bike bikebike",
+            "name" : "Mr-White"
+        },
+        "options" : {
+            "email":"testEmail@gmail.com",
+            "city":"Lodz"
+        }
+    },{
+        "login": "loginTest1",
+        "avatar":"avatar.jpg",
+        "bike": {
+            "photos": [
+                "http://www.pedalroom.com/p/mr-fix-it-7819_1.jpg",
+                "http://image.made-in-china.com/2f0j00kMltwycaljrO/700c-New-Model-Hot-Sale-Specialized-White-Fixed-Gear-Bike-ZLF-2013S-.jpg",
+                "http://www.czytajniepytaj.pl/data/domains/1/pl/filemanager_files/accounts/lena/3/rower/pure-fix-cycles-bikes-4.jpg",
+                "http://static.jensonusa.com/images/Default-Image/Zoom/4/BI242F00.jpg"
+            ],
+            "desc" : "Awesome bike bike bike bikebike",
+            "name" : "Mr-White"
+        },
+        "options" : {
+            "email":"testEmail@gmail.com",
+            "city":"Lodz"
+        }
+    },{
+        "login": "loginTest1",
+        "avatar":"avatar.jpg",
+        "bike": {
+            "photos": [
+                "http://www.pedalroom.com/p/mr-fix-it-7819_1.jpg",
+                "http://image.made-in-china.com/2f0j00kMltwycaljrO/700c-New-Model-Hot-Sale-Specialized-White-Fixed-Gear-Bike-ZLF-2013S-.jpg",
+                "http://www.czytajniepytaj.pl/data/domains/1/pl/filemanager_files/accounts/lena/3/rower/pure-fix-cycles-bikes-4.jpg",
+                "http://static.jensonusa.com/images/Default-Image/Zoom/4/BI242F00.jpg"
+            ],
+            "desc" : "Awesome bike bike bike bikebike",
+            "name" : "Mr-White"
+        },
+        "options" : {
+            "email":"testEmail@gmail.com",
+            "city":"Lodz"
+        }
+    },{
+        "login": "loginTest1",
+        "avatar":"avatar.jpg",
+        "bike": {
+            "photos": [
+                "http://www.pedalroom.com/p/mr-fix-it-7819_1.jpg",
+                "http://image.made-in-china.com/2f0j00kMltwycaljrO/700c-New-Model-Hot-Sale-Specialized-White-Fixed-Gear-Bike-ZLF-2013S-.jpg",
+                "http://www.czytajniepytaj.pl/data/domains/1/pl/filemanager_files/accounts/lena/3/rower/pure-fix-cycles-bikes-4.jpg",
+                "http://static.jensonusa.com/images/Default-Image/Zoom/4/BI242F00.jpg"
+            ],
+            "desc" : "Awesome bike bike bike bikebike",
+            "name" : "Mr-White"
+        },
+        "options" : {
+            "email":"testEmail@gmail.com",
+            "city":"Lodz"
+        }
+    },{
+        "login": "loginTest1",
+        "avatar":"avatar.jpg",
+        "bike": {
+            "photos": [
+                "http://www.pedalroom.com/p/mr-fix-it-7819_1.jpg",
+                "http://image.made-in-china.com/2f0j00kMltwycaljrO/700c-New-Model-Hot-Sale-Specialized-White-Fixed-Gear-Bike-ZLF-2013S-.jpg",
+                "http://www.czytajniepytaj.pl/data/domains/1/pl/filemanager_files/accounts/lena/3/rower/pure-fix-cycles-bikes-4.jpg",
+                "http://static.jensonusa.com/images/Default-Image/Zoom/4/BI242F00.jpg"
+            ],
+            "desc" : "Awesome bike bike bike bikebike",
+            "name" : "Mr-White"
+        },
+        "options" : {
+            "email":"testEmail@gmail.com",
+            "city":"Lodz"
+        }
+    }
+    ];
+
+}]);
+
+
+
+
